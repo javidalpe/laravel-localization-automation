b7a9850722074cdb4d89ed68a11c9a97bf63a075
Javier Vidal <javier.vidal@sngular.team>
2017-11-02 12:37:54 +0100

    Add Laravel provider

---
+++ src/LaravelLocalizationAutomationServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Javidalpe\LaravelLocalizationAutomation;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLocalizationAutomationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
